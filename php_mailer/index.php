3a9c83dad7139c9e6e6b189f531b1a08e65608ac
supercharger05 <36926458+supercharger05@users.noreply.github.com>
2020-03-06 18:33:55 +0800

    contact.php 5

--- php_mailer/index.php
+++ php_mailer/index.php
@@ -1,33 +1,28 @@
 <?php
+	require 'phpmailer/PHPMailerAutoload.php';
+	$mail = new PHPMailer;
+
+	//$mail->isSMTP();
+	$mail->Host='smtp.gmail.com';
+	$mail->Port=587;
+	$mail->SMTPAuth=true;
+	$mail->SMTPSecure='tls';
 	
-	$result="";
+	$mail->Username='studyinszu@gmail.com';
+	$mail->Password='Tidza1983';
 
-	if (isset($_POST['submit'])) {
-		require 'phpmailer/PHPMailerAutoload.php';
-		$mail = new PHPMailer;
+	$mail->setFrom('studyinszu@gmail.com','Berdi');
+	$mail->addAddress('contact@100fontan.com');
+	$mail->addReplyTo('studyinszu@gmail.com');
 
-		//$mail->isSMTP();
-		$mail->Host='smtp.gmail.com';
-		$mail->Port=587;
-		$mail->SMTPAuth=true;
-		$mail->SMTPSecure='tls';
-		
-		$mail->Username='studyinszu@gmail.com';
-		$mail->Password='Tidza1983';
+	$mail->isHTML(true);
+	$mail->Subject='PHP Mailer Subject';
+	$mail->Body='<h1 align=center>Subscribe My Chaneel</h1><br><h4 align=center>Like this Video</h4>';
 
-		$mail->setFrom('studyinszu@gmail.com','Berdi');
-		$mail->addAddress('kanayamaki@outlook.com');
-		$mail->addReplyTo('studyinszu@gmail.com');
-
-		$mail->isHTML(true);
-		$mail->Subject='PHP Mailer Subject';
-		$mail->Body='<h1 align=center>Subscribe My Chaneel</h1><br><h4 align=center>Like this Video</h4>';
-
-		if (!$mail->send()) {
-			echo "Message could not be sent";
-		}
-		else{
-			echo "Message has been sent!";
-		}
+	if (!$mail->send()) {
+		echo "Message could not be sent";
+	}
+	else{
+		echo "Message has been sent!";
 	}
 ?>
