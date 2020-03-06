cff6f28e408da1b31b005b9c197aa89e515fef90
supercharger05 <36926458+supercharger05@users.noreply.github.com>
2020-03-06 18:10:24 +0800

    contact.php 1

--- contact.php
+++ contact.php
@@ -4,28 +4,30 @@
 
 	if (isset($_POST['submit'])) {
 		require 'phpmailer/PHPMailerAutoload.php';
-		$mail = new PHPMailer();
+		$mail = new PHPMailer;
 
+		$mail->isSMTP();
 		$mail->Host='smtp.gmail.com';
 		$mail->Port=587;
 		$mail->SMTPAuth=true;
 		$mail->SMTPSecure='tls';
+		
 		$mail->Username='studyinszu@gmail.com';
 		$mail->Password='Tidza1983';
 
-		$mail->setFrom($_POST['email'],$_POST['name']);
+		$mail->setFrom('studyinszu@gmail.com','Berdi');
 		$mail->addAddress('kanayamaki@outlook.com');
-		$mail->addReplyTo($_POST['email'],$_POST['name']);
+		$mail->addReplyTo('studyinszu@gmail.com');
 
 		$mail->isHTML(true);
-		$mail->Subject='Greeting';
-		$mail->Body='My name is Motta';
+		$mail->Subject='PHP Mailer Subject';
+		$mail->Body='<h1 align=center>Subscribe My Chaneel</h1><br><h4 align=center>Like this Video</h4>';
 
 		if (!$mail->send()) {
-			$result="Something went wrong. Please try again.";
+			echo "Message could not be sent";
 		}
 		else{
-			$result="Thanks ".$_POST['name']." for contacting us. We'll get back to you soon!";
+			echo "Message has been sent!";
 		}
 	}
 ?>
